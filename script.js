5284a404ffb8c3523d175b71138a175b185fdfc7
Vinzon <luongvinson@gmail.com>
2022-06-08 10:44:18 -0600

    get first number, get operator

--- script.js
+++ script.js
@@ -18,27 +18,37 @@ function operate(operator, a, b){
     return operator(a,b)
 }
 
-let tempNumber;
-let firstNumber;
-let secondNumber;
+let displayValue =''
+let currentOperator = ''
+let firstNumber = '';
+let secondNumber ='';
 
-// listen & return temp number
+// listen for numbers and display
 const numbers = document.querySelectorAll('.number');
+
 numbers.forEach(number => {
     number.addEventListener('click', function(e){       
-        tempNumber = parseInt(e.target.innerHTML);
-        document.getElementById('result').innerHTML += tempNumber;
+        displayValue += e.target.innerHTML;
+        document.getElementById('result').innerHTML += e.target.innerHTML;
         });
+})
+
+// listen for operators, record display as first number, get current operator
+const operators = document.querySelectorAll('.operator')
+
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) =>{
+        firstNumber = parseFloat(displayValue);
+        currentOperator = e.target.id;
+        console.log(firstNumber)
+        console.log(currentOperator)
+    })
 })
 
 // clear and reset numbers
 const clear = document.getElementById('clear')
 clear.addEventListener('click', () => {
-    firstNumber = null;
-    secondNumber = null;
+    firstNumber = '';
+    secondNumber = '';
     document.getElementById('result').innerHTML = '';
 })
-
-
-// store this AFTER operand clicked
-        // firstNumber = parseInt(e.target.innerHTML);
