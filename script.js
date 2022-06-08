2aac96a65481f0c48cc0603539c484919419d7ee
Vinzon <luongvinson@gmail.com>
2022-06-07 21:46:02 -0600

    get and store first number, enable clear button, display numbers when pressed, display zero when cleared

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ function divide(a, b){
 function operate(operator, a, b){
     return operator(a,b)
 }
+
+let firstNumber;
+let secondNumber;
+
+// listen & return numbers
+const numbers = document.querySelectorAll('.number');
+
+numbers.forEach(number => {
+    number.addEventListener('click', function(e){       
+        firstNumber = parseInt(e.target.innerHTML);
+        document.getElementById('result').innerHTML = firstNumber
+        });
+})
+
+// clear and reset numbers
+const clear = document.getElementById('clear')
+clear.addEventListener('click', () => {
+    firstNumber = null;
+    secondNumber = null;
+    document.getElementById('result').innerHTML = 0;
+})
